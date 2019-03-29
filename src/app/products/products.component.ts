da288e16bacf36bb7d385bc8ef867ccaeddf8e04
Siddhu <siddharthaesunuri@gmail.com>
2019-03-29 16:11:48 +0530

    first commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title:string = 'Products List';
+  showImage:boolean = false;
+  ToggleImage(): void{
+    this.showImage = !this.showImage;
+  };
+  products:any[] = [
+    {
+      'Name':'Santoor',
+      'Price':'55',
+      'date':'01-01-2019',
+      'rating':'4.5',
+      'img':'https://via.placeholder.com/50',
+    },
+    {
+      'Name':'Lux',
+      'Price':'45',
+      'date':'01-02-2019',
+      'rating':'5',
+      'img':'https://via.placeholder.com/50',
+    },
+    {
+      'Name':'Medmix',
+      'Price':'75',
+      'date':'01-05-2019',
+      'rating':'2.5',
+      'img':'https://via.placeholder.com/50',
+    }
+  ];
+
+
+}
