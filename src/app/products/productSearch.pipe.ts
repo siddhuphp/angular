f06465b19898aa499714fa87ce8e82b5466cc895
siddhu <siddharthaesunuri@gmail.com>
2019-04-01 21:58:11 +0530

    search value done

--- src/app/products/productSearch.pipe.ts
+++ src/app/products/productSearch.pipe.ts
@@ -7,6 +7,6 @@ import { Pipe, PipeTransform  } from "@angular/core";
 export class ProductSearchPipe implements PipeTransform {
     transform(value: any[], userInput: string) {
         userInput = userInput?userInput.toLowerCase():null;
-        return userInput ? value.filter((product) => { product.Name.toLowerCase().indexOf(userInput) !== -1}):value;
+        return userInput ? value.filter((product) =>  product.Name.toLowerCase().indexOf(userInput) !== -1):value;
     }
 }
